d633cf39f7447c51e0ab9738358e78b06afb603d
jerluc <me@jerluc.com>
2018-12-12 12:33:05 -0800

    Improving bulk index performance with parallelism

--- cmd/esx/index.go
+++ cmd/esx/index.go
@@ -5,58 +5,37 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/olivere/elastic"
+	"golang.org/x/sync/errgroup"
 	"io"
 	"os"
 	"strings"
 )
 
-func doIndex(client *elastic.Client) error {
-	dec := json.NewDecoder(os.Stdin)
-	var batch = client.Bulk()
-	for {
-		var doc map[string]interface{}
-		err := dec.Decode(&doc)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		docId := doc[*docIdField]
-		if docId == nil {
-			return fmt.Errorf("Document ID field [%s] is not set on document: %+v", *docIdField, doc)
-		}
-		for k, _ := range doc {
-			// Ignore all fields that start with an underscore
-			if strings.HasPrefix(k, "_") {
-				delete(doc, k)
+type Batch []map[string]interface{}
+
+func indexWorker(ctx context.Context, client *elastic.Client, batches <-chan Batch) error {
+	for batch := range batches {
+		bulk := client.Bulk()
+		for _, doc := range batch {
+			docId := doc[*docIdField]
+			if docId == nil {
+				return fmt.Errorf("Document ID field [%s] is not set on document: %+v", *docIdField, doc)
 			}
-		}
-		req := elastic.NewBulkIndexRequest().
-			Type("doc_as_upsert").
-			Index(*esIndex).
-			Type(*esType).
-			Id(fmt.Sprintf("%v", docId)).
-			Doc(doc)
-		batch.Add(req)
-		if batch.NumberOfActions() == *batchSize {
-			res, err := batch.Do(context.Background())
-			if err != nil {
-				return err
-			}
-			failed := res.Failed()
-			if len(failed) > 0 {
-				for _, failure := range failed {
-					fmt.Fprintf(os.Stderr, "%s: %v\n", failure.Id, failure.Error)
+			for k, _ := range doc {
+				// Ignore all fields that start with an underscore
+				if strings.HasPrefix(k, "_") {
+					delete(doc, k)
 				}
-				return fmt.Errorf("Batch failed: %+v", failed)
 			}
-			batch = client.Bulk()
+			req := elastic.NewBulkIndexRequest().
+				OpType(*indexAction).
+				Index(*esIndex).
+				Type(*esType).
+				Id(fmt.Sprintf("%v", docId)).
+				Doc(doc)
+			bulk.Add(req)
 		}
-	}
-	// Flush any remaining documents
-	if batch.NumberOfActions() > 0 {
-		res, err := batch.Do(context.Background())
+		res, err := bulk.Do(ctx)
 		if err != nil {
 			return err
 		}
@@ -69,4 +48,44 @@ func doIndex(client *elastic.Client) error {
 		}
 	}
 	return nil
+}
+
+func producer(ctx context.Context, client *elastic.Client, batches chan<- Batch) error {
+	dec := json.NewDecoder(os.Stdin)
+	var batch []map[string]interface{}
+	for {
+		var doc map[string]interface{}
+		err := dec.Decode(&doc)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		batch = append(batch, doc)
+		if len(batch) == *batchSize {
+			batches <- batch
+			batch = Batch{}
+		}
+	}
+	// Flush any remaining documents
+	if len(batch) > 0 {
+		batches <- batch
+	}
+	close(batches)
+	return nil
+}
+
+func doIndex(client *elastic.Client) error {
+	g, ctx := errgroup.WithContext(context.Background())
+	batches := make(chan Batch, (*numWorkers)*2)
+	for i := 0; i < *numWorkers; i++ {
+		g.Go(func() error {
+			return indexWorker(ctx, client, batches)
+		})
+	}
+	g.Go(func() error {
+		return producer(ctx, client, batches)
+	})
+	return g.Wait()
 }
--- cmd/esx/main.go
+++ cmd/esx/main.go
@@ -8,13 +8,14 @@ import (
 	"runtime"
 )
 
+const WorkersPerCPU = 2
+
 var (
 	esHost        = kingpin.Flag("es-host", "ElasticSearch host:port").Short('H').Envar("ES_HOST").Default("localhost:9200").String()
 	esIndex       = kingpin.Flag("es-index", "ElasticSearch index to use").Short('I').Envar("ES_INDEX").Required().String()
 	esType        = kingpin.Flag("es-type", "ElasticSearch doc type to use").Short('D').Envar("ES_TYPE").Default("_doc").String()
 	esTimeout     = kingpin.Flag("es-timeout", "ElasticSearch operation timeout duration").Short('T').Default("10s").Duration()
 	debug         = kingpin.Flag("debug", "Debug mode").Short('d').Bool()
-	parallelism   = kingpin.Flag("parallelism", "Number of cores to use").Short('p').Default("0").Int()
 	progress      = kingpin.Flag("progress", "Report progress").Short('P').Bool()
 	scrollCmd     = kingpin.Command("scroll", "Scrolls an ElasticSearch index")
 	scrollSize    = scrollCmd.Flag("scroll-size", "ElasticSearch scroll size").Short('s').Default("100").Int()
@@ -22,6 +23,8 @@ var (
 	queryFile     = scrollCmd.Flag("query-file", "Query JSON file").Short('f').File()
 	queryStr      = scrollCmd.Flag("query", "Query string").Short('q').String()
 	indexCmd      = kingpin.Command("index", "Indexes data into an ElasticSearch index")
+	indexAction   = indexCmd.Flag("index-action", "Index action type (\"index\" or \"update\")").Short('a').Default("index").String()
+	numWorkers    = indexCmd.Flag("index-workers", "Number of index workers").Short('w').Default("0").Int()
 	batchSize     = indexCmd.Flag("batch-size", "Number of documents to batch index").Short('b').Default("100").Int()
 	docIdField    = indexCmd.Flag("doc-id-field", "JSON field to use as document ID").Short('i').Default("_id").String()
 	ProgressBar   = &Progress{}
@@ -37,16 +40,20 @@ func handleErr(cmd string, err error) {
 func main() {
 	cmd := kingpin.Parse()
 
-	if *parallelism < 1 {
-		*parallelism = runtime.NumCPU()
+	numCores := runtime.NumCPU()
+	if *numWorkers < 1 {
+		*numWorkers = WorkersPerCPU * numCores
 	}
-	runtime.GOMAXPROCS(*parallelism)
+	runtime.GOMAXPROCS(numCores)
 
 	if *progress {
 		ProgressBar.Enable()
 	}
 
-	client, err := elastic.NewClient(elastic.SetURL(fmt.Sprintf("http://%s", *esHost)))
+	client, err := elastic.NewClient(
+		elastic.SetURL(fmt.Sprintf("http://%s", *esHost)),
+		elastic.SetGzip(true),
+	)
 	if err != nil {
 		panic(err)
 	}
